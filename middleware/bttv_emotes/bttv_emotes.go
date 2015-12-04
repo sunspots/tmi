0cec60b77ef40152a17eae89a9cd3653d557abb9
Vidar Westfelt <vidar@westfelt.se>
2015-12-04 23:38:36 +0100

    Update to fit format

--- middleware/bttv_emotes/bttv_emotes.go
+++ middleware/bttv_emotes/bttv_emotes.go
@@ -11,39 +11,32 @@ import (
 	"github.com/SunspotsEU/tmi"
 )
 
-var SPACE = regexp.MustCompile(`\s`)
+var space = regexp.MustCompile(`\s`)
 
-// Emote, unmarshaled from API
-type AdvEmote struct {
+// BTTVEmote is unmarshaled from the BTTV API
+type BTTVEmote struct {
 	ID        string `json:"id"`
 	Code      string `json:"code"`
 	Channel   string `json:"channel"`
 	Regex     *regexp.Regexp
 	ImageType string `json:"imageType"`
 }
-
-type EmoteInMessage struct {
-	Emote *AdvEmote
-	Pos   [][]int
+// BTTVEmoteSet is used to unmarshal sets from the BTTV API
+type BTTVEmoteSet struct {
+	Status      int          `json:"status"`
+	Emotes      []*BTTVEmote `json:"emotes"`
+	URLTemplate string       `json:"urlTemplate"`
 }
-
-type BttvEmoteSet struct {
-	Status      int         `json:"status"`
-	Emotes      []*AdvEmote `json:"emotes"`
-	UrlTemplate string      `json:"urlTemplate"`
+// BTTVEmotes is also used for unmarshalling sets from the BTTV API
+type BTTVEmotes struct {
+	Sets map[string]*BTTVEmoteSet
 }
-
-type BttvEmotes struct {
-	Sets map[string]*BttvEmoteSet
-}
-
-func (bttv *BttvEmotes) Init() {
-	bttv.Sets = map[string]*BttvEmoteSet{}
-
+// Init sets up the BTTV data and downloads emotes
+func (bttv *BTTVEmotes) Init() {
 	bttv.DownloadEmotes()
 }
 
-func (bttv *BttvEmotes) Download(url string, setName string) {
+func (bttv *BTTVEmotes) Download(url string, setName string) {
 	// download all emotes
 
 	res, err := http.Get(url)
@@ -57,7 +50,7 @@ func (bttv *BttvEmotes) Download(url string, setName string) {
 		log.Println("Error fetching BTTV Emotes:", err)
 		return
 	}
-	var response BttvEmoteSet
+	var response BTTVEmoteSet
 	err = json.Unmarshal(body, &response)
 	if err != nil {
 		log.Println("Error Unmarshaling BTTV Emotes:", err)
@@ -70,17 +63,17 @@ func (bttv *BttvEmotes) Download(url string, setName string) {
 	log.Println("Fetched", len(response.Emotes), "bttv emotes for", setName)
 	bttv.Sets[setName] = &response
 }
-func (bttv *BttvEmotes) DownloadChannelEmotes(channel string) {
+func (bttv *BTTVEmotes) DownloadChannelEmotes(channel string) {
 	bttv.Download("https://api.betterttv.net/2/channels/"+channel, channel)
 	bttv.MakeEmoteRegexps(bttv.Sets[channel])
 }
 
-func (bttv *BttvEmotes) DownloadEmotes() {
+func (bttv *BTTVEmotes) DownloadEmotes() {
 	bttv.Download("https://api.betterttv.net/2/emotes", "bttv")
 	bttv.MakeEmoteRegexps(bttv.Sets["bttv"])
 }
 
-func (bttv *BttvEmotes) MakeEmoteRegexps(emoteRes *BttvEmoteSet) {
+func (bttv *BTTVEmotes) MakeEmoteRegexps(emoteRes *BTTVEmoteSet) {
 	if emoteRes == nil {
 		return
 	}
@@ -98,7 +91,7 @@ func (bttv *BttvEmotes) MakeEmoteRegexps(emoteRes *BttvEmoteSet) {
 	}
 }
 
-func (bttv *BttvEmotes) MatchEmotes(m *tmi.Message) []*tmi.Emote {
+func (bttv *BTTVEmotes) MatchEmotes(m *tmi.Message) []*tmi.Emote {
 	foundEmotes := []*tmi.Emote{}
 
 	for set, emoteRes := range bttv.Sets {
@@ -118,12 +111,12 @@ func (bttv *BttvEmotes) MatchEmotes(m *tmi.Message) []*tmi.Emote {
 				continue
 			}
 			for _, pos := range found {
-				if SPACE.Match([]byte(m.Trailing[pos[0] : pos[0]+1])) {
-					pos[0] += 1
+				if space.Match([]byte(m.Trailing[pos[0] : pos[0]+1])) {
+					pos[0]++
 				}
 				if pos[1] != len(m.Trailing) {
-					if SPACE.MatchString(m.Trailing[pos[1]-1 : pos[1]]) {
-						pos[1] -= 1
+					if space.MatchString(m.Trailing[pos[1]-1 : pos[1]]) {
+						pos[1]--
 					}
 				}
 				foundEmotes = append(foundEmotes, &tmi.Emote{
@@ -138,10 +131,15 @@ func (bttv *BttvEmotes) MatchEmotes(m *tmi.Message) []*tmi.Emote {
 	return foundEmotes
 }
 
+
 // TBD
-func (bttv *BttvEmotes) MiddleWare(m *tmi.Message, err error) (*tmi.Message, error) {
+func (bttv *BTTVEmotes) MiddleWare(m *tmi.Message, err error) (*tmi.Message, error) {
 	if err != nil {
 		return m, err
 	}
 	return m, nil
+}
+
+func New() *BTTVEmotes{
+	return &BTTVEmotes{Sets: map[string]*BTTVEmoteSet{}}
 }
