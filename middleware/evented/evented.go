456b542ca869e6db8a50999994997d746c03b126
Vidar Westfelt <vidar@westfelt.se>
2016-06-20 21:40:25 +0200

    naming, formatting

--- middleware/evented/evented.go
+++ middleware/evented/evented.go
@@ -6,13 +6,14 @@ import (
 	"github.com/chuckpreslar/emission"
 	"github.com/sunspots/tmi"
 )
-// EventedConnection wraps a tmi Connection together with an EventEmitter
-type EventedConnection struct {
-  *emission.Emitter
-  *tmi.Connection
+
+// Connection wraps a tmi Connection together with an EventEmitter
+type Connection struct {
+	*emission.Emitter
+	*tmi.Connection
 }
 
-func readLoop(connection *tmi.Connection, emitter *EventedConnection) {
+func readLoop(connection *tmi.Connection, emitter *Connection) {
 	for {
 		evt, err := connection.ReadMessage()
 		if err != nil {
@@ -20,15 +21,15 @@ func readLoop(connection *tmi.Connection, emitter *EventedConnection) {
 			// handle reconnects, but we're just gonna error out for the sake of simplicity.
 			log.Fatal(err)
 		} else {
-      emitter.Emit("message", evt)
-      emitter.Emit(evt.Command, evt)
+			emitter.Emit("message", evt)
+			emitter.Emit(evt.Command, evt)
 		}
 	}
 }
 
 // New instantiates an EventEmittter
-func New(tmi *tmi.Connection) *EventedConnection {
-	emitter := &EventedConnection{emission.NewEmitter(), tmi}
-  go readLoop(tmi, emitter)
+func New(tmi *tmi.Connection) *Connection {
+	emitter := &Connection{emission.NewEmitter(), tmi}
+	go readLoop(tmi, emitter)
 	return emitter
 }
