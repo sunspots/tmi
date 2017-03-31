e58030dca6b7c5f8543f6432420997a527f5a8de
Vidar Westfelt <vidar@westfelt.se>
2017-03-31 22:37:17 +0200

    Restructure

---
+++ routines.go
@@ -0,0 +1,129 @@
+package tmi
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+)
+
+// The main loop to read messages from the server, runs as a goroutine.
+func (tmi *Connection) readLoop() {
+	defer func() {
+		log.Println("Reader closed")
+		tmi.Done()
+	}()
+	br := bufio.NewReaderSize(tmi.socket, maxMessageSize)
+
+	for {
+		select {
+		case <-tmi.end:
+			return
+		default:
+			if tmi.socket != nil {
+				tmi.socket.SetReadDeadline(time.Now().Add(tmi.Timeout*2 + tmi.KeepAlive))
+			}
+			msg, err := br.ReadString('\n')
+			if err != nil {
+				tmi.Error <- err
+				return
+			}
+			if tmi.socket != nil {
+				var zero time.Time
+				tmi.socket.SetReadDeadline(zero)
+			}
+			tmi.lastMessage = time.Now()
+
+			if tmi.Debug {
+				dbg.Print("< ", msg)
+			}
+
+			message := ParseMessage(msg)
+			if message.Command == "PING" {
+				tmi.Send("PONG " + message.Trailing)
+				continue
+			}
+			tmi.MessageChan <- message
+		}
+	}
+}
+
+func (tmi *Connection) writeLoop() {
+	defer func() {
+		log.Println("Writer closed")
+		tmi.Done()
+	}()
+	for {
+		select {
+		case s, ok := <-tmi.send:
+			if !ok {
+				tmi.Error <- errors.New("send channel closed")
+				return
+			}
+			if tmi.socket == nil {
+				tmi.Error <- errors.New("no socket to write to")
+				return
+			}
+			if s == "" {
+				continue
+			}
+			if tmi.Debug {
+				dbg.Printf("> %s\n", s)
+			}
+			tmi.socket.SetWriteDeadline(time.Now().Add(tmi.Timeout))
+			_, err := fmt.Fprintf(tmi.socket, "%s\r\n", s)
+			var zero time.Time
+			tmi.socket.SetWriteDeadline(zero)
+
+			if err != nil {
+				tmi.Error <- err
+				return
+			}
+		case <-tmi.end:
+			return
+		}
+	}
+}
+
+func (tmi *Connection) pingLoop() {
+	defer func() {
+		log.Println("Pinger stopped")
+		tmi.Done()
+	}()
+	ticker := time.NewTicker(tmi.Timeout) // Tick for monitoring
+	for {
+		select {
+		case _, ok := <-ticker.C:
+			if !ok {
+				// The ticker has been closed, probably shouldn't happen before tmi.end is closed
+				log.Println("ticker not ok")
+				return
+			}
+
+			//Ping if we haven't received anything from the server within the keep alive period
+			if time.Since(tmi.lastMessage) >= tmi.KeepAlive {
+				tmi.Sendf("PING %d", time.Now().UnixNano())
+			}
+		case <-tmi.end:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+func (tmi *Connection) controlLoop() {
+	for !tmi.Stopped() {
+		err := <-tmi.Error
+		if tmi.Stopped() {
+			// If one of the goroutines errors when we've already stopped,
+			// There's no need to handle the error.
+			continue
+		}
+		log.Printf("Error, disconnecting: %s\n", err)
+		tmi.Done()
+		log.Println("controlLoop done, waiting for disconnect")
+		tmi.Disconnect()
+	}
+	log.Println("controlloop terminated")
+}
--- tmi.go
+++ tmi.go
@@ -2,7 +2,6 @@
 package tmi
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"log"
@@ -34,6 +33,25 @@ func (tmi *Connection) Sendf(format string, a ...interface{}) {
 	tmi.Send(fmt.Sprintf(format, a...))
 }
 
+// Join simply sends a join message
+func (tmi *Connection) Join(channel string) {
+	tmi.Send("JOIN " + channel)
+}
+
+// Stopped tells us wether the client is stopped or not
+func (tmi *Connection) Stopped() bool {
+	tmi.Lock()
+	defer tmi.Unlock()
+	return tmi.stopped
+}
+
+// Update the stopped status when starting or stopping the client
+func (tmi *Connection) setStopped(value bool) {
+	tmi.Lock()
+	tmi.stopped = value
+	tmi.Unlock()
+}
+
 // ReadMessage reads an incoming message from the server, blocking until a message is recieved
 func (tmi *Connection) ReadMessage() (*Message, error) {
 	evt, ok := <-tmi.MessageChan
@@ -57,7 +75,6 @@ func (tmi *Connection) Disconnect() {
 		tmi.socket = nil
 		close(tmi.MessageChan)
 		dbg.Println("Disconnected!")
-		//tmi.Error <- errors.New("Disconnect Called")
 	}
 }
 
@@ -67,146 +84,7 @@ func (tmi *Connection) Reconnect() error {
 	return tmi.Connect()
 }
 
-// Stopped tells us wether the client is stopped or not
-func (tmi *Connection) Stopped() bool {
-	tmi.Lock()
-	defer tmi.Unlock()
-	return tmi.stopped
-}
-
-// Update the stopped status when starting or stopping the client
-func (tmi *Connection) setStopped(value bool) {
-	tmi.Lock()
-	tmi.stopped = value
-	tmi.Unlock()
-}
-
-// The main loop to read messages from the server, runs as a goroutine.
-func (tmi *Connection) readLoop() {
-	defer func() {
-		log.Println("Reader closed")
-		tmi.Done()
-	}()
-	br := bufio.NewReaderSize(tmi.socket, maxMessageSize)
-
-	for {
-		select {
-		case <-tmi.end:
-			return
-		default:
-			if tmi.socket != nil {
-				tmi.socket.SetReadDeadline(time.Now().Add(tmi.Timeout*2 + tmi.KeepAlive))
-			}
-			msg, err := br.ReadString('\n')
-			if err != nil {
-				tmi.Error <- err
-				return
-			}
-			if tmi.socket != nil {
-				var zero time.Time
-				tmi.socket.SetReadDeadline(zero)
-			}
-			tmi.lastMessage = time.Now()
-
-			if tmi.Debug {
-				dbg.Print("< ", msg)
-			}
-
-			message := ParseMessage(msg)
-			if message.Command == "PING" {
-				tmi.Send("PONG " + message.Trailing)
-				continue
-			}
-			tmi.MessageChan <- message
-		}
-	}
-}
-
-func (tmi *Connection) writeLoop() {
-	defer func() {
-		log.Println("Writer closed")
-		tmi.Done()
-	}()
-	for {
-		select {
-		case s, ok := <-tmi.send:
-			if !ok {
-				tmi.Error <- errors.New("send channel closed")
-				return
-			}
-			if tmi.socket == nil {
-				tmi.Error <- errors.New("no socket to write to")
-				return
-			}
-			if s == "" {
-				continue
-			}
-			if tmi.Debug {
-				dbg.Printf("> %s\n", s)
-			}
-			tmi.socket.SetWriteDeadline(time.Now().Add(tmi.Timeout))
-			_, err := fmt.Fprintf(tmi.socket, "%s\r\n", s)
-			var zero time.Time
-			tmi.socket.SetWriteDeadline(zero)
-
-			if err != nil {
-				tmi.Error <- err
-				return
-			}
-		case <-tmi.end:
-			return
-		}
-	}
-}
-
-func (tmi *Connection) pingLoop() {
-	defer func() {
-		log.Println("Pinger stopped")
-		tmi.Done()
-	}()
-	ticker := time.NewTicker(tmi.Timeout) // Tick for monitoring
-	for {
-		select {
-		case _, ok := <-ticker.C:
-			if !ok {
-				// The ticker has been closed, probably shouldn't happen before tmi.end is closed
-				log.Println("ticker not ok")
-				return
-			}
-
-			//Ping if we haven't received anything from the server within the keep alive period
-			if time.Since(tmi.lastMessage) >= tmi.KeepAlive {
-				tmi.Sendf("PING %d", time.Now().UnixNano())
-			}
-		case <-tmi.end:
-			ticker.Stop()
-			return
-		}
-	}
-}
-
-func (tmi *Connection) controlLoop() {
-	for !tmi.Stopped() {
-		err := <-tmi.Error
-		if tmi.Stopped() {
-			// If one of the goroutines errors when we've already stopped,
-			// There's no need to handle the error.
-			continue
-		}
-		log.Printf("Error, disconnecting: %s\n", err)
-		tmi.Done()
-		log.Println("controlLoop done, waiting for disconnect")
-		tmi.Disconnect()
-	}
-	log.Println("controlloop terminated")
-}
-
-// Join joins a channel
-func (tmi *Connection) Join(channel string) {
-	tmi.Send("JOIN " + channel)
-}
-
-// Connect connects to the server, starts reading and authenticates
+// Connect connects to the server, starts routines and authenticates
 func (tmi *Connection) Connect() (err error) {
 
 	if len(tmi.Token) > 0 && tmi.Token[0:6] == "oauth:" {
@@ -267,6 +145,7 @@ func Connect(nick, token string) *Connection {
 	return new
 }
 
+// Anonymous is a shortcut to create a new connection without credentials/auth, and connecting as with Connect
 func Anonymous() *Connection {
 	new := New("justinfan999", "")
 	new.Connect()
